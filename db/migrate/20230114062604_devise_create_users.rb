d1baafd5518031620739d61dbbb20885855f29ae
Wendel <123527715+Yukiro21@users.noreply.github.com>
2023-01-31 08:06:57 +0800

    Update 20230114062604_devise_create_users.rb

--- db/migrate/20230114062604_devise_create_users.rb
+++ db/migrate/20230114062604_devise_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviceCreateUsers < ActiveRecord::Migration[7.0]
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
